f3549e4a13ead3873d5e068fa1cb1cd862b1f36a
iCarlSO <91926524+icarlso8@users.noreply.github.com>
2025-07-23 10:47:00 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// 👉 Este archivo contiene la configuración del proyecto Firebase
+// ⚠️ NO contiene datos sensibles. apiKey puede estar expuesta.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAA7SZYGbCZrBpurLVrW4RPbM5Zl-32Mog",
+  authDomain: "trkkn-cot-firebase.firebaseapp.com",
+  projectId: "trkkn-cot-firebase",
+  storageBucket: "trkkn-cot-firebase.firebasestorage.app",
+  messagingSenderId: "490397786735",
+  appId: "1:490397786735:web:6cc8d158f904865bdc239a"
+};
