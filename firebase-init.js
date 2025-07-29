13e5d90c25681429d981787471fc32db4ca27f78
iCarlSO <91926524+icarlso8@users.noreply.github.com>
2025-07-29 11:24:29 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,6 +1,6 @@
 // firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAA7SZYGbCZrBpurLVrW4RPbM5Zl-32Mog",
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: "1:490397786735:web:6cc8d158f904865bdc239a"
 };
 
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+// const auth = getAuth(app);
 window.auth = auth; // ✅ lo hacemos global para que sea accesible desde otros scripts
